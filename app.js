5f02a5c1351848f24ebb1da4d97b928be21f57b0
Aleksandr <95400829+Dorozhkin-Aleksandr@users.noreply.github.com>
2022-03-14 19:34:56 +0700

    add registrarion

--- app.js
+++ app.js
@@ -1,53 +1,213 @@
 const express = require('express')
-// достаём функцию экспресс из библиотеки
+// достаём пакет экспресс для создания сервера
 const path = require('path')
-// формирует путь к файлам
+// подключаем пакет path для работы с путями в файловой системе
+
 const { db } = require('./DB.js')
 // импортируем объект с БД из файла
-const server = express()
-// в переменной server экземпляр сервера
-const PORT = 3000
+const { dbUser } = require('./DBuser.js')
+// импортируем объект с БД пользователей из файла
+const { sessions } = require('./sessions')
+// импортируем объект с пользовательскими сессиями из файла sessions.js
 
+const { checkAuth } = require('./src/middlewares/checkAuth.js')
+// импортируем объект файл checkAuth (там у нас проверка автоизации пользователя перед входом на страницу блога)
+
+const cookieParser = require('cookie-parser')
+// установили пакет куки парсер, нужен для работы с куками
+
+const server = express()
+// создали экземпляр сервера
+const PORT = 3000
+// выбрали 3000 порт
+
+const hbs = require('hbs')
+const { request } = require('http')
+const { response } = require('express')
+const res = require('express/lib/response')
+//достаём пакет hbs
+
+//const sessions = {} //сюда будем добавлять информацию о вошедшем пользователе
+//в итоге решили его убрать в отдельный файл, оставил здесь для себя в качестве примера
 
 
 server.set('view engine', 'hbs')
 // подключил шаблонизатор HBS
-
 server.set('views', path.join(__dirname, 'src', 'views'))
 // подключил папку с "усами" (views, src - папки с hbs, __dirname - путь до папки с проектом, определяется динамически)
+// я папку views подключаю с применением __dirname потому, что (process.env.PWD, 'src', 'views') не стал работать на моей тетовой системе
+hbs.registerPartials(path.join(__dirname, 'src', 'views', 'partials'))
+// папка partials для частичных шаблонов, у нас это заголовок сайта с навигацией
 
 
 server.use(express.urlencoded({ extended: true }))
 // без этой строки сервер не будет принимать данные из пользовательской формы
 // она должна распологаться раньше, чем описание "слушателя"
 
+server.use(cookieParser())
+// учим экспресс принимать данные из куки (подключаем пакет как мидлвару)
+
+
+
+
+
+
+
+// глобальная мидл вара для всего приложения
+server.use((request, response, next) => {
+    const sidFromUser = request.cookies.sid
+    //взяли айдишник сессии из куки
+    const currentSession = sessions[sidFromUser]
+
+    if (currentSession) {
+        const currentUser = dbUser.users.find((user) => user.email == currentSession.email)
+        //вытащили из БД пользователей почту текущего юзера
+        response.locals.name = currentUser.name
+        //для  передачи почты текущего пользователя в страницу ХБС
+        //альтернативный вариант передать данные  вторым аргументом в объекте response.render('ххх' {ЗДЕСЬ})
+
+        //по аналогии вытащу имя пользователя для блога, чтобы ему не приходилоь его повторно набирать
+    }
+    next()
+})
+
+
+
+
+
+
 
 server.get('/', function (request, response) {
-    const userQueryParameters = request.query //сохраняет в объекте запрос пользователя
+    response.render('main')
+})
+
+
+
+
+
+
+// Этот большой блок отвечает за регистрацию пользователя, срабатывает в случае перехода пользователя в раздел SignUp
+server.get('/auth/signup', (request, response) => {
+    response.render('signUp')
+})
+// эта часть блока отвечает за приём данных из пользовательской формы регистрации на сайте
+// данные, которые пользовательотправил через запрос POST перейдут в массив (массив хранится в оперативке ПК)
+// далее сервер формирует сессию и куку для пользователя
+// в самом низу происъодит редирект на главгую страницу
+server.post('/auth/signup', (request, response) => {
+    const { name, email, password } = request.body
+    dbUser.users.push({
+        name,
+        email,
+        password,
+    })
+
+    const sid = Date.now() // формируем уникальный ключ sid - session id, Date.now - количество мс с 1970 года
+    sessions[sid] = {
+        email,
+    }
+
+    response.cookie('sid', sid, {
+        httpOnly: true,  //ограничили доступ к куке через JS
+        maxAge: 120000,
+    }) //в () название и значение, которое буде содержать кука и настройки куки
+
+    response.redirect('/') //редиректим пользователя на главную страницу (для метода пост нельзя применять метод render)
+}) //для принятия данных из формы signUp в объект dbUser
+
+
+
+
+
+
+
+// Этот большой блок отвечает за ВХОД пользователя через SignIn
+// этот блок кода практически идентичен вышестоящему SignUp
+server.get('/auth/signin', (request, response) => {
+    response.render('signIn')
+})
+
+server.post('/auth/signin', (request, response) => {
+    const { email, password } = request.body
+
+    //получили данные из формы, теперь нужно проверить есть ли такой пользователь в нашей БД
+    const currentUser = dbUser.users.find((user) => user.email == email)
+    if (currentUser) {
+
+        if (currentUser.password == password) {
+            const sid = Date.now()
+            sessions[sid] = {
+                email,
+            }
+
+            response.cookie('sid', sid, {
+                httpOnly: true,
+                maxAge: 120000,
+            })
+
+            return response.redirect('/')
+        }
+
+    }
+    response.redirect('/auth/signup')
+    //так как у нас рядом два redirect, то перед первым нужно указать нашей функции return, иначе они могут последовательно выполнится
+})
+
+
+
+
+
+
+//в этом блоке описан выход пользователя из системы
+server.get('/auth/signout', (request, response) => {
+    const sidFromUserCookie = request.cookies.sid
+    //получили идентификатор сессии от пользователя (sid - это имя нашей куки)
+
+    delete sessions[sidFromUserCookie]
+    //удаляем данные пользователя из хранилища (на сервере)
+
+    response.clearCookie('sid')
+    //команда браузеру, чтобы он почистил куку
+    response.redirect('/')
+    //отправляем пользователя на главную страницу
+
+})
+
+
+
+
+
+
+
+// этот большой блок отвечает за переход на страницу блога
+server.get('/blog', checkAuth, function (request, response) {
+    // checkAuth проверит авторизован ли пользователь
+    //const userQueryParameters = request.query //сохраняет в объекте запрос пользователя это было нужно для qw параметров
     let messagesForRender = db.messages
 
-    if (userQueryParameters.limit !== undefined && Number.isNaN(+userQueryParameters.limit) === false) {
-        messagesForRender = db.messages.slice(0, userQueryParameters.limit)
-    }
-    if (userQueryParameters.reverse !== undefined && userQueryParameters.reverse === "true") {
-        messagesForRender = messagesForRender.reverse()
-    }
-
-
-    response.render('main', { listOfMessages: messagesForRender }) // main - название файла с Hbs кодом
-}) // db.messages из импортированнного объекта
-// обработчик с колбэк функцией (внутри объект запроса и объект ответа),обработчиков мб много
+    response.render('blog', { listOfMessages: messagesForRender })
+    // отправляем на рендер страницу блога и объект с сообщениями пользователя из второй БД
+})
 
 server.post('/messagebook', (request, response) => {
     const dataFromForm = request.body
 
-    //console.log({ dataFromForm })
     db.messages.push(dataFromForm)
+    // db.messages.name = name
 
-    response.redirect('/')
+    // console.log(dataFromForm)
+    // console.log(db.messages)
+    ///////////////где то здесь надо добавить в объет name
+    response.redirect('/blog')
 })
-// 
 
+
+
+
+
+
+
+//этот блок отвечает на левые запросы пользователя, если он будет вводить некорректный адрес страницы
 server.get('*', (request, response) => {   //* -значит любая абра-кадабра)
     response.render('404.hbs')
 })
@@ -58,4 +218,4 @@ server.get('*', (request, response) => {   //* -значит любая абра
 server.listen(PORT, () => {
     console.log(`Server has been started on port: ${PORT}`)
 })
-// сервер будет работать на 3000 порту, всего доступно 2 в 16 степени портов
+// запустили наш сервер на 3000 порту
